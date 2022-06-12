b36a876666fa027dc5d5f029d50fb545f3d74a79
Bogdan Bebić <bogdanbebic@gmail.com>
2022-06-12 18:53:16 +0200

    Implement game menu

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,23 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import scala.annotation.tailrec
 
-def msg = "I was compiled by Scala 3. :)"
+@main def bloxorz: Unit =
+  gameLoop
+
+val usage: String = """Choose menu option:
+  1 <filepath>: Load map from file
+  2: Start game from loaded map
+  3: Create new map from loaded map
+  0: Exit game"""
+
+@tailrec def gameLoop: Unit =
+  println(usage)
+  val input = io.StdIn.readLine()
+  input match {
+    case s"1 $filepath" => println(s"Load map from '$filepath'")
+    case "2"            => println("Start game")
+    case "3"            => println("Create new map")
+    case "0"            => return
+    case option         => println(s"Unrecognized menu option: '$option'")
+  }
+
+  gameLoop
