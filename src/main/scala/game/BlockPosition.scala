84e8734a5d67cf4c82c3bcf8bcac74c2a1117a32
Bogdan Bebić <bogdanbebic@gmail.com>
2023-01-31 14:19:22 +0100

    Implement moveBlock

--- src/main/scala/game/BlockPosition.scala
+++ src/main/scala/game/BlockPosition.scala
@@ -10,7 +10,96 @@ case class Position(row: Int, col: Int)
 case class BlockPosition(upperLeft: Position, additional: Option[Position])
 
 def moveBlock(block: BlockPosition, move: Move): BlockPosition = {
-  BlockPosition(Position(0, 0), None)
+  val col = block.upperLeft.col
+  val row = block.upperLeft.row
+
+  block.additional match {
+    case None => {
+      move match
+        case UpMove => {
+          return BlockPosition(
+            Position(row - 2, col),
+            Some(Position(row - 1, col))
+          )
+        }
+        case DownMove => {
+          return BlockPosition(
+            Position(row + 1, col),
+            Some(Position(row + 2, col))
+          )
+        }
+        case LeftMove => {
+          return BlockPosition(
+            Position(row, col - 2),
+            Some(Position(row, col - 1))
+          )
+        }
+        case RightMove => {
+          return BlockPosition(
+            Position(row, col + 1),
+            Some(Position(row, col + 2))
+          )
+        }
+    }
+
+    case Some(additional) => {
+      if (row == additional.row) {
+        // block is left to right
+        move match
+          case UpMove => {
+            return BlockPosition(
+              Position(row - 1, col),
+              Some(Position(additional.row - 1, additional.col))
+            )
+          }
+          case DownMove => {
+            return BlockPosition(
+              Position(row + 1, col),
+              Some(Position(additional.row + 1, additional.col))
+            )
+          }
+          case LeftMove => {
+            return BlockPosition(
+              Position(row, col - 1),
+              None
+            )
+          }
+          case RightMove => {
+            return BlockPosition(
+              Position(row, col + 2),
+              None
+            )
+          }
+      } else {
+        // block is up to down
+        move match
+          case UpMove => {
+            return BlockPosition(
+              Position(row - 1, col),
+              None
+            )
+          }
+          case DownMove => {
+            return BlockPosition(
+              Position(row + 2, col),
+              None
+            )
+          }
+          case LeftMove => {
+            return BlockPosition(
+              Position(row, col - 1),
+              Some(Position(additional.row, additional.col - 1))
+            )
+          }
+          case RightMove => {
+            return BlockPosition(
+              Position(row, col + 1),
+              Some(Position(additional.row, additional.col + 1))
+            )
+          }
+      }
+    }
+  }
 }
 
 def getOutcome(
--- src/test/scala/BlockPositionSuite.scala
+++ src/test/scala/BlockPositionSuite.scala
@@ -4,11 +4,97 @@ import org.scalatest.flatspec.AnyFlatSpec
 import game.{Position, BlockPosition}
 import game.{getOutcome, moveBlock}
 
-import game.{ValidMove, OutOfBounds, GameWon, FellThrough}
+import game.{UpMove, DownMove, LeftMove, RightMove}
 
+import game.{ValidMove, OutOfBounds, GameWon, FellThrough}
 import map.{RegularTile, StartTile, TargetTile, NoTile, FallthroughTile}
 
 class BlockPositionSuite extends AnyFlatSpec {
+  "moveBlock" should "return new block position for UpMove when vertical" in {
+    val block = BlockPosition(Position(3, 3), None)
+    val expected = BlockPosition(Position(1, 3), Some(Position(2, 3)))
+
+    assert(moveBlock(block, UpMove) == expected)
+  }
+
+  it should "return correct new block position for DownMove when vertical" in {
+    val block = BlockPosition(Position(3, 3), None)
+    val expected = BlockPosition(Position(4, 3), Some(Position(5, 3)))
+
+    assert(moveBlock(block, DownMove) == expected)
+  }
+
+  it should "return correct new block position for LeftMove when vertical" in {
+    val block = BlockPosition(Position(3, 3), None)
+    val expected = BlockPosition(Position(3, 1), Some(Position(3, 2)))
+
+    assert(moveBlock(block, LeftMove) == expected)
+  }
+
+  it should "return correct new block position for RightMove when vertical" in {
+    val block = BlockPosition(Position(3, 3), None)
+    val expected = BlockPosition(Position(3, 4), Some(Position(3, 5)))
+
+    assert(moveBlock(block, RightMove) == expected)
+  }
+
+  it should "return correct new block position for UpMove when horizontal left-right" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(3, 4)))
+    val expected = BlockPosition(Position(2, 3), Some(Position(2, 4)))
+
+    assert(moveBlock(block, UpMove) == expected)
+  }
+
+  it should "return correct new block position for DownMove when horizontal left-right" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(3, 4)))
+    val expected = BlockPosition(Position(4, 3), Some(Position(4, 4)))
+
+    assert(moveBlock(block, DownMove) == expected)
+
+  }
+
+  it should "return correct new block position for LeftMove when horizontal left-right" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(3, 4)))
+    val expected = BlockPosition(Position(3, 2), None)
+
+    assert(moveBlock(block, LeftMove) == expected)
+  }
+
+  it should "return correct new block position for RightMove when horizontal left-right" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(3, 4)))
+    val expected = BlockPosition(Position(3, 5), None)
+
+    assert(moveBlock(block, RightMove) == expected)
+  }
+
+  it should "return correct new block position for UpMove when horizontal up-down" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(4, 3)))
+    val expected = BlockPosition(Position(2, 3), None)
+
+    assert(moveBlock(block, UpMove) == expected)
+  }
+
+  it should "return correct new block position for DownMove when horizontal up-down" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(4, 3)))
+    val expected = BlockPosition(Position(5, 3), None)
+
+    assert(moveBlock(block, DownMove) == expected)
+  }
+
+  it should "return correct new block position for LeftMove when horizontal up-down" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(4, 3)))
+    val expected = BlockPosition(Position(3, 2), Some(Position(4, 2)))
+
+    assert(moveBlock(block, LeftMove) == expected)
+  }
+
+  it should "return correct new block position for RightMove when horizontal up-down" in {
+    val block = BlockPosition(Position(3, 3), Some(Position(4, 3)))
+    val expected = BlockPosition(Position(3, 4), Some(Position(4, 4)))
+
+    assert(moveBlock(block, RightMove) == expected)
+  }
+
   "getOutcome" should "return ValidMove when block vertical on regular tile" in {
     val block = BlockPosition(Position(0, 0), None)
     val map = Vector(Vector(RegularTile))
